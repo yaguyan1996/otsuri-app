60b2c972cc5550489f570a511c14edfd7fe537b9
yaguyan1996 <yaguyan1996@gmail.com>
2026-06-18 07:21:40 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,18 @@
-const CACHE = 'otsuri-v1';
+const CACHE = 'otsuri-v2';
 const FILES = ['./'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
